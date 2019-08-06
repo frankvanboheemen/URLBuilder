5c4a87773a532c80c0233b9e4b65a82691a8014a
Frank van Boheemen <frankvb@gmail.com>
2019-08-06 11:02:22 +0200

    Initial Commit

---
+++ URLBuilder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLBuilder
+//
+//  Created by Frank van Boheemen on 06/08/2019.
+//  Copyright © 2019 Frank van Boheemen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
