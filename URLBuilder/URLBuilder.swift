9b9adec80e98ad8426f481d118b6490248a3f445
Frank van Boheemen <frankvb@gmail.com>
2019-08-06 15:13:16 +0200

    Setting up URLBuilder as a custom class

---
+++ URLBuilder/URLBuilder.swift
@@ -0,0 +1,36 @@
+//
+//  URLBuilder.swift
+//  URLBuilder
+//
+//  Created by Frank van Boheemen on 06/08/2019.
+//  Copyright © 2019 Frank van Boheemen. All rights reserved.
+//
+
+import Foundation
+
+class URLBuilder {
+    class func buildURL(scheme: String, host: String, queryItems: [String : String]) -> URL? {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = host
+        components.queryItems = []
+        for item in queryItems {
+            components.queryItems?.append(URLQueryItem(name: item.key, value: item.value))
+        }
+        return components.url
+    }
+    
+    class func getQueryItems(from url: URL) -> [String : String] {
+        let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+
+        var queryItems = [String: String]()
+        if let retreivedQueryItems = components?.queryItems {
+            for item in retreivedQueryItems {
+                if let value = item.value {
+                    queryItems[item.name] = value
+                }
+            }
+        }
+        return queryItems
+    }
+}
--- URLBuilderTests/URLBuilderTests.swift
+++ URLBuilderTests/URLBuilderTests.swift
@@ -11,23 +11,23 @@ import XCTest
 
 class URLBuilderTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testRetreiveQueryItemsFromCustomURLScheme() {
+        let lattitude = 52.3680
+        let longitude = 4.9036
+        let url = URLBuilder.buildURL(scheme: "locationApp", host: "place", queryItems: ["lattitude" : "\(lattitude)", "longitude": "\(longitude)"])
+        
+        if let url = url {
+            let items = URLBuilder.getQueryItems(from: url)
+            
+            if let stringValue = items["lattitude"],
+                let retreivedLattitude = Double(stringValue){
+                XCTAssert(lattitude == retreivedLattitude)
+            }
+            
+            if let stringValue = items["longitude"],
+                let retreivedLongitude = Double(stringValue){
+                XCTAssert(longitude == retreivedLongitude)
+            }
         }
     }
 
